8cc47a7bebad4167f3e96b5168846f01742cf6e3
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-11-06 13:41:26 +1100

    removed Comments about functions

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 // FUNCTION ADD
 function add(a, b){ 
-    return a + b;
-  }
-  // let firstAddition = add(10, 150);
-  // console.log(firstAddition)
-  
-  // FUNCTION SUB
-  function sub(a, b){
-    return a - b;
-  }
-  // let firstSub = sub(100, 10);
-  // console.log (firstSub);
-  
-  // FUNCTION MULTIPLY
-  function mult(a, b){
-    return a * b;
-  }
-  // let firstMult = mult(10, 15);
-  // console.log(firstMult);
-  
-  // FUNCTION DIVIDE
-  function divide(a, b){
-    return a / b;
-  }
-  // let firstDivide = divide(100, 3)
-  // console.log(firstDivide);
-  
-  let firstNumber
-  let operator
-  let secondNumber
-  
-  
+  return a + b;
+}
+// FUNCTION SUB
+function sub(a, b){
+  return a - b;
+}
+// FUNCTION MULTIPLY
+function mult(a, b){
+  return a * b;
+}
+// FUNCTION DIVIDE
+function divide(a, b){
+  return a / b;
+}
+
+let firstNumber
+let operator
+let secondNumber
+
+
