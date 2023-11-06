856987ac5022f9071ea583491f2ea49bbdd6b46b
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-11-06 13:36:39 +1100

    4 base math functions added

---
+++ script.js
@@ -0,0 +1,33 @@
+// FUNCTION ADD
+function add(a, b){ 
+    return a + b;
+  }
+  // let firstAddition = add(10, 150);
+  // console.log(firstAddition)
+  
+  // FUNCTION SUB
+  function sub(a, b){
+    return a - b;
+  }
+  // let firstSub = sub(100, 10);
+  // console.log (firstSub);
+  
+  // FUNCTION MULTIPLY
+  function mult(a, b){
+    return a * b;
+  }
+  // let firstMult = mult(10, 15);
+  // console.log(firstMult);
+  
+  // FUNCTION DIVIDE
+  function divide(a, b){
+    return a / b;
+  }
+  // let firstDivide = divide(100, 3)
+  // console.log(firstDivide);
+  
+  let firstNumber
+  let operator
+  let secondNumber
+  
+  
